26f543dea7018e6cd6544bb6e41d0de941734a8e
MartínBuenoLópez <martin.buneo@upb.edu.co>
2025-05-17 23:02:12 -0500

    Interfaz

--- Numerator.java
+++ Numerator.java
@@ -1,5 +1,25 @@
 import java.util.Scanner;
 public class Numerator{
     public static void main(String[] args){
+    
+    }
+
+    public static void Interfaz(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("NUMERATOR 2");
+        System.out.println("MAS NUMERATOR QUE NUNCA");
+        System.out.println("Presione 1 para continuar");
+        System.out.println("Presione 0 para salir");
+        int inicio=scanner.nextInt();
+        while(inicio!=1 && inicio!=0) {
+            System.out.println("Ingrese un comando valido");
+            inicio=scanner.nextInt();
+        }
+        if(inicio==1){
+            //Función del Juego
+        }else if(inicio==0){
+            System.out.println("Saliendo de Numerator...");
+        }
+        scanner.close();
     }
 }
