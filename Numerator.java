2b38aae9e94a1eb2aec55d3bfa592f9aeefed8ab
tomas <tomascuestalopez2305gmail.com>
2025-05-17 23:49:40 -0500

    juego

--- Numerator.java
+++ Numerator.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class Numerator{
     public static void main(String[] args){
-    
+    Interfaz();
     }
 
-    public static void Interfaz(String[] args) {
+    public static void Interfaz() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("NUMERATOR 2");
         System.out.println("MAS NUMERATOR QUE NUNCA");
@@ -16,10 +16,26 @@ public class Numerator{
             inicio=scanner.nextInt();
         }
         if(inicio==1){
-            //Función del Juego
+            Juego();        
         }else if(inicio==0){
             System.out.println("Saliendo de Numerator...");
         }
         scanner.close();
-    }
+    }public static void Juego(){
+Scanner scanner = new Scanner(System.in);
+int numero = (int)(Math.random() * 50);
+int adivinanza = scanner.nextInt();
+while(adivinanza!=numero){
+if(numero<adivinanza){
+System.out.println("Muy grande!!!");
+adivinanza= scanner.nextInt();
+}else if(numero>adivinanza){
+System.out.println("Muy poquito!!!!");
+adivinanza= scanner.nextInt();
+}
+}
+//fin();
+scanner.close();
+}
+
 }
